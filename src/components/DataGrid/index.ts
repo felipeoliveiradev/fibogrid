4b537944f7495c6fe9af5d44c21e3f05f142b5c4
felipeoliveiradev <felipeoliveiradeveloper@gmail.com>
2025-12-08 09:19:29 +0000

    Changes

--- src/components/DataGrid/index.ts
+++ src/components/DataGrid/index.ts
@@ -3,6 +3,7 @@ export * from './types';
 export * from './utils/helpers';
 export { GridProvider, useGridContext } from './context/GridContext';
 export * from './utils/grouping';
+export { exportToExcel } from './utils/excelExport';
 
 // Hooks exports for advanced usage
 export { useGridState } from './hooks/useGridState';
@@ -13,9 +14,14 @@ export { useRowDrag } from './hooks/useRowDrag';
 export { useRangeSelection } from './hooks/useRangeSelection';
 export { useKeyboardNavigation } from './hooks/useKeyboardNavigation';
 export { useGrouping } from './hooks/useGrouping';
+export { useCellSelection } from './hooks/useCellSelection';
+export { useUndoRedo } from './hooks/useUndoRedo';
+export { useFillHandle } from './hooks/useFillHandle';
 
 // Components exports for custom compositions
 export { GridToolbar } from './components/GridToolbar';
 export { GridStatusBar } from './components/GridStatusBar';
 export { ColumnMenu } from './components/ColumnMenu';
 export { GroupRow } from './components/GroupRow';
+export { ColumnPanel } from './components/ColumnPanel';
+export { FilterPanel } from './components/FilterPanel';
